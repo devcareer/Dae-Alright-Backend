ee26078e6a783ef968d09800faf3133cef9bc37c
Desmond Edem <edemintech@gmail.com>
2019-10-24 11:11:36 +0100

    chore(middleware): implement validation middleware (#4)

---
+++ src/middleware/validator.js
@@ -0,0 +1,24 @@
+import { matchedData, validationResult } from 'express-validators';
+import response from '../helpers/response';
+
+export default (schemas) => {
+  const validatorCheck = (req, res, next) => {
+    const errors = validationResult(req);
+    req = { ...req, ...matchedData(req) };
+
+    if (!errors.isEmpty()) {
+      const mapErrors = Object
+        .entries(errors.mapped())
+        .reduce(
+          (accumulator, [key, value]) => {
+            accumulator[key] = value.msg;
+            return accumulator;
+          }, {}
+        );
+      response.setError(400, mapErrors);
+      return response.send(res);
+    }
+    return next();
+  };
+  return [...(schemas.length && [schemas]), validatorCheck];
+};
