ddc829a35266a49d77e0a34826fe42a6d9fa99f2
Sijuade Ajagunna <mahyor.sam@gmail.com>
2019-11-21 09:17:59 +0100

    finalized social login
    refeactor social auth

---
+++ src/config/passport-config/facebook-passport-config.js
@@ -0,0 +1,42 @@
+import { config } from 'dotenv';
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import database from '../../database/models';
+import { findBySocialID } from './config';
+
+const { User } = database;
+
+config();
+
+passport.use(new FacebookStrategy.Strategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: '/auth/facebook/redirect/',
+  profileFields: ['id', 'emails', 'name'],
+},
+(accessToken, refreshToken, profile, done) => {
+  try {
+    findBySocialID(profile.id, 'facebook').then(async currentUser => {
+      if (currentUser) {
+        return done(null, currentUser);
+      }
+      const profileObj = profile._json;
+      const user = {
+        socialID: profile.id,
+        provider: 'facebook',
+        firstName: profileObj.first_name,
+        lastName: profileObj.last_name,
+        email: profileObj.email || '',
+        phone: '',
+        address: '',
+      };
+
+      await User.create(user);
+      return done(null, user);
+    });
+  } catch (err) {
+    return done(err, false);
+  }
+},),);
+
+export default passport;
