4011454b9891e694c6e4075d1c737a71ca62e423
Tunde Ajagbe <tundeajagbea@gmail.com>
2019-11-22 03:24:14 +0100

    fix fb signin and passport jwt bug
    fix code format
    
    fix code restructure

--- src/config/passport-config/config.js
+++ src/config/passport-config/config.js
@@ -4,29 +4,32 @@ import database from '../../database/models';
 
 const { User } = database;
 
-export const findBySocialID = async (socialID, provider) => User.findOne({
-  where: { socialID, provider }
-});
+export const findBySocialID = async (socialID, provider) =>
+  User.findOne({
+    where: { socialID, provider },
+  });
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_KEY,
 };
 
-passport.use(new Strategy(options, (jwtPayload, done) => {
-  User.findOne({ where: { socialID: jwtPayload.socialID, provider: jwtPayload.provider } })
-    .then(user => {
-      if (user) {
-        done(null, user);
-      } else {
-        done(null, false);
-      }
-    })
-    .catch(err => {
-      if (err) {
-        return done(err, false);
-      }
-    });
-}),);
+passport.use(
+  new Strategy(options, (jwtPayload, done) => {
+    User.findOne({ where: { socialID: jwtPayload.socialID, email: jwtPayload.email } })
+      .then(user => {
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      })
+      .catch(err => {
+        if (err) {
+          return done(err, false);
+        }
+      });
+  }),
+);
 
 export default passport;
